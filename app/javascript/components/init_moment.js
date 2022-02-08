898742ae69fb65598e533dde3c9f9aa184c2a3cc
Saïf <saifbacari@gmail.com>
2022-02-08 08:47:56 +0100

    maj files

--- app/javascript/components/init_moment.js
+++ app/javascript/components/init_moment.js
@@ -1,10 +1,10 @@
 import moment from 'moment';
 
 const initMoment = () => {
-const date = moment('Thu Oct 25 2018 17:30:03 GMT+0300').fromNow();
+const date = moment("2018-10-25 17:30 +0300", "YYYY-MM-DD HH:mm Z").fromNow();
 
-console.log(date);
 }
 
 
 export { initMoment };
+
